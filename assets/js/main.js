168e9ee2140ac79247e8c4baa0f8cd58ec92c646
visaka <visidevi@gmail.com>
2018-02-15 00:00:59 -0300

    data

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,10 +35,24 @@ fetch('https://pokeapi.co/api/v2/pokemon/5/')
     console.log(data);
 
     // Let's make some HTML!
-    let html = `<h2><a href="${data.html_url}">${data.login}</a></h2>
+    let html =  `<div class="card small">
+    <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="${data.sprites.back_default}">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">${data.name}<i class="material-icons right">more_vert</i></span>
+      <p><a href="#">This is a link</a></p>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+      <p>TIPO:${data.types[0].type.name}</p>
+    </div>
+    </div>
+  </div> `/*`<h2><a href="${data.login}">${data.species.name}</a></h2>
       <p>${data.name}</p>
       <p>Followers: ${data.followers}</p>
-    `;
+    `;*/
 
     // Put that HTML on the page
     myGif.innerHTML = html;
