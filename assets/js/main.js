d720de6ec15b277be795b97f048f6baed7ec5dd1
Viviana Marcelo <32303287+skyblue16@users.noreply.github.com>
2018-02-15 09:10:50 -0300

    Revert "veve"

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,32 @@
-const search = document.getElementById('search_btn')
-search.addEventListener('click', function(){
+// $('#search_btn').click(function(event) {
+//   console.log('Entro');
+//   $('#myGif').empty();
+//   var gif = $('#textarea1').val();
+//   $('#titleMargin').hide();
+//   ajaxGif(gif);
+//   $('#textarea1').val('');
+// });
 
-const gif = document.getElementById('textarea1').value;
+// function ajaxGif(gif) {
+//   $.ajax({
+//   	url: 'https://api.giphy.com/v1/gifs/search?',
+//     type: 'GET',
+//     datatype: 'json',
+//     data: {
+//       q: gif,
+//       api_key: '2MDlExiyLnzP1YyQ2suDZfXL63sfFTwM'
+//     }
+//   }).done(function(response) {
+//     console.log(response);
+//     dibujarGifs(response.data);
+//   })
+// 	 .fail(function() {
+//       console.log('error');
+//     });
+// };
+
 const myGif = document.getElementById('myGif');
-fetch(`https://pokeapi.co/api/v2/pokemon/${gif}/`)
+fetch('https://pokeapi.co/api/v2/pokemon/5/')
   .then(function(response) {
     // Turns the the JSON into a JS object
     return response.json();
@@ -12,21 +35,11 @@ fetch(`https://pokeapi.co/api/v2/pokemon/${gif}/`)
     console.log(data);
 
     // Let's make some HTML!
-    let html =  `<div class="col s3">
-    <div class="card">
-    <div class="card-image waves-effect waves-block waves-light">
-      <img class="activator" src="${data.sprites.back_default}">
-    </div>
-    <div class="card-content">
-      <span class="card-title activator grey-text text-darken-4">${data.name}<i class="material-icons right">more_vert</i></span>
-      <p><a href="#">This is a link</a></p>
-    </div>
-    <div class="card-reveal">
-      <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-      <p>TIPO:${data.types[0].type.name}</p>
-    </div>
-    </div>
-  </div> `
+    let html = `<h2><a href="${data.html_url}">${data.login}</a></h2>
+      <p>${data.name}</p>
+      <p>Followers: ${data.followers}</p>
+    `;
+
+    // Put that HTML on the page
     myGif.innerHTML = html;
   });
-});
